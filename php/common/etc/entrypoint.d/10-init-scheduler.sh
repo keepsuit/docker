5d61e33fa9df3474d54d9eaf1dd2f4e5d43c410f
Fabio Capucci <fabio.cappuc@gmail.com>
2026-06-20 11:06:26 +0200

    improved worker script

--- php/common/etc/entrypoint.d/10-init-scheduler.sh
+++ php/common/etc/entrypoint.d/10-init-scheduler.sh
@@ -4,7 +4,11 @@ set -e
 
 # If command is "php artisan schedule:work", we override it with supercronic
 if [ "$1" = "php" ] && [ "$2" = "artisan" ] && [ "$3" = "schedule:work" ]; then
-    echo "* * * * * cd $APP_BASE_DIR && php artisan schedule:run" > /tmp/crontab
+    crontab="/tmp/crontab"
 
-    echo "/usr/local/bin/supercronic -quiet -passthrough-logs /tmp/crontab" > /tmp/docker_cmd_override
+    cat > "$crontab" <<EOF
+* * * * * cd "$APP_BASE_DIR" && php artisan schedule:run --no-interaction 2>&1 | grep -v "No scheduled commands are ready to run"
+EOF
+
+    echo "/usr/local/bin/supercronic -quiet -passthrough-logs $crontab" > /tmp/docker_cmd_override
 fi
