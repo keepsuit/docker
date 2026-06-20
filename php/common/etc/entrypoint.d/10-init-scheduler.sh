b016dae486ed843c9413830b61930fad71e41796
Fabio Capucci <fabio.cappuc@gmail.com>
2026-06-20 11:46:40 +0200

    fix script

--- php/common/etc/entrypoint.d/10-init-scheduler.sh
+++ php/common/etc/entrypoint.d/10-init-scheduler.sh
@@ -2,13 +2,13 @@
 
 set -e
 
+SUPERCRONIC_CRONTAB="/tmp/crontab"
+
 # If command is "php artisan schedule:work", we override it with supercronic
 if [ "$1" = "php" ] && [ "$2" = "artisan" ] && [ "$3" = "schedule:work" ]; then
-    crontab="/tmp/crontab"
-
-    cat > "$crontab" <<EOF
+    cat > "$SUPERCRONIC_CRONTAB" <<EOF
 * * * * * cd "$APP_BASE_DIR" && php artisan schedule:run --no-interaction 2>&1 | grep -v "No scheduled commands are ready to run"
 EOF
 
-    echo "/usr/local/bin/supercronic -quiet -passthrough-logs $crontab" > /tmp/docker_cmd_override
+    echo "/usr/local/bin/supercronic -quiet -passthrough-logs $SUPERCRONIC_CRONTAB" > /tmp/docker_cmd_override
 fi
